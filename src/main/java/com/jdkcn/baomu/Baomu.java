78f11216808f1dfb9a49afd9ba75c20ac7ffee3b
Rory Ye <rory.cn@gmail.com>
2014-09-04 00:02:58 +0800

    add project structure

---
+++ src/main/java/com/jdkcn/baomu/Baomu.java
@@ -0,0 +1,18 @@
+/**
+ * Project:baomu
+ * File:Baomu.java
+ * Copyright 2004-2014 Homolo Co., Ltd. All rights reserved.
+ */
+package com.jdkcn.baomu;
+
+/**
+ * @author Rory
+ * @date Sep 3, 2014
+ * @version $Id$
+ */
+public class Baomu {
+
+	public static void main(String... args) {
+		
+	}
+}
---
+++ src/main/java/com/jdkcn/baomu/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Project:baomu
+ * File:package-info.java
+ * Copyright 2004-2014 Homolo Co., Ltd. All rights reserved.
+ */
+/**
+ * @author Rory
+ * @date Sep 3, 2014
+ * @version $Id$
+ */
+package com.jdkcn.baomu;
